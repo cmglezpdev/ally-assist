5bd62625f6dcd67d50e6d7cd7718e6cd9a656293
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2025-08-09 16:05:55 -0400

    feature(convex): add convex package

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,29 @@
-import { Button } from "@workspace/ui/components/button"
+'use client'
+import { useMutation, useQuery } from "convex/react";
+import { api } from "@workspace/backend/_generated/api";
+import { Button } from '@workspace/ui/components/button';
 
 export default function Page() {
+  const users = useQuery(api.users.getMany);
+  const addUser = useMutation(api.users.add);
+
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm">Button</Button>
-      </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <h1 className="text-2xl font-bold mb-3">apps/web</h1>
+
+      <form onSubmit={async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target as HTMLFormElement);
+        const name = formData.get("name") as string;
+        await addUser({ name });
+      }}>
+        <input type="text" className="border border-gray-300 rounded-md p-2" name="name" />
+        <Button type="submit">Add User</Button>
+      </form>
+
+
+
+      <pre>{JSON.stringify(users, null, 2)}</pre>
     </div>
   )
 }
